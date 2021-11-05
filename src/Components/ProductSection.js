2a6e5f2a53aba75fd4b3983701eb0367d7a809a6
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-11-05 14:28:49 -0500

    adding changes

--- src/Components/ProductSection.js
+++ src/Components/ProductSection.js
@@ -19,12 +19,9 @@ export const ProductSection = () => {
       <div className="card-body" key={item.id}>
         <h2 className="card-title">{item.name}</h2>
         <img className="card-img" src={item.img} alt="img-product"/>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <input onChange={(e) => handleAddItem(e, item.stock)}/>
+        <input onChange={(e) => handleAddItem(e, item.stock)}/>+
         <button className="btn btn-primary">
-          add to cart
+        <i class="fas fa-shopping-cart"></i>add to cart
         </button>
       </div>
         ))}
